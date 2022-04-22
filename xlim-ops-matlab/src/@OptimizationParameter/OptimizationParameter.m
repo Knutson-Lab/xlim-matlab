fcbec50879412cc4f177dcc72b13251f03c93e03
shureed <shureed.qazi@nih.gov>
2022-04-22 17:58:39 -0400

    dimensions and nargin>1

--- xlim-ops-matlab/src/@OptimizationParameter/OptimizationParameter.m
+++ xlim-ops-matlab/src/@OptimizationParameter/OptimizationParameter.m
@@ -3,9 +3,9 @@ classdef OptimizationParameter
 
     properties
         % VALUE
-        Value
+        Value (1,1)
         % CONSTRAINTS
-        Constraints
+        Constraints (1,1)
     end
     
     methods
@@ -15,17 +15,22 @@ classdef OptimizationParameter
                 val (:,1) FitParameter
                 const (:,1) BoundaryConstraints
             end
-            noofval = numel(val);
-            noofconst = numel(const);
-            assert(noofval == noofconst, "Must be equal number of FitParameter objects and BoundaryConstraint objects");
-            param(1:noofval) = OptimizationParameter; 
-            for i=1:noofval
-                param(i).Value = val(i);
-                param(i).Constraints = const(i);
-            end            
+            if(nargin>1)
+                noofval = numel(val);
+                noofconst = numel(const);
+                assert(noofval == noofconst, "Must be equal number of FitParameter objects and BoundaryConstraint objects");
+
+                
+
+                param(1:noofval) = OptimizationParameter; 
+                for i=1:noofval
+                    param(i).Value = val(i);
+                    param(i).Constraints = const(i);
+                end 
+            end
         end
     end
-    
+
     methods (Abstract)
         multiparam = multiSet(multiparam, args)
         varargout = multiGet(multiparam, prop)
