b3836cd22e4eecd4c60a2e16cf6d5a56dbb26174
shureed <shureed.qazi@nih.gov>
2022-05-04 13:16:45 -0400

    boundary constraint can be scalar

--- xlim-ops-matlab/src/@OptimizationParameter/OptimizationParameter.m
+++ xlim-ops-matlab/src/@OptimizationParameter/OptimizationParameter.m
@@ -18,28 +18,43 @@ classdef OptimizationParameter
             if(nargin>1)
                 noofval = numel(val);
                 noofconst = numel(const);
-                assert(noofval == noofconst, "Must be equal number of FitParameter objects and BoundaryConstraint objects");
+                assert(noofval == noofconst || noofconst == 1, "Must be equal number of FitParameter objects and BoundaryConstraint objects");
 
                 values = val.multiGet("Value");
                 bounds = const.multiGet("ScalarBounds");
 
                 [lb,ub] = bounds.multiGet("LowerBound","UpperBound");
-                assert(numel(lb) == numel(ub),"Number of lower bounds and upper bounds must be equal");
                 noofbounds = numel(lb);
                 
-                if(noofbounds~=0)
-                    assert(noofbounds == values,"Each bound must correspond to a value")
+                if(~isempty(lb) && ~isempty(ub))
+                    assert(noofbounds == values || noofbounds == 1,"Each bound must correspond to a value")
 
                     checklower = all(lb<=values);
                     checkupper = all(values<=ub);                
 
                     assert(checklower && checkupper,"Some Value is not between ScalarBounds");
+                elseif(isempty(lb) && ~isempty(ub))
+                    assert(noofbounds == values || noofbounds == 1,"Each bound must correspond to a value")
+
+                    checkupper = all(values<=ub);                
+
+                    assert(checkupper,"Some Value is not between ScalarBounds");
+                elseif(~isempty(lb) && isempty(ub))
+                    assert(noofbounds == values || noofbounds == 1,"Each bound must correspond to a value")
+
+                    checklower = all(lb<=values);              
+
+                    assert(checklower,"Some Value is not between ScalarBounds");
                 end
 
                 param(1:noofval) = OptimizationParameter; 
                 for i=1:noofval
                     param(i).Value = val(i);
-                    param(i).Constraints = const(i);
+                    if(noofconst==1)
+                        param(i).Constraints = const;
+                    else 
+                        param(i).Constraints = const(i);
+                    end    
                 end 
             end
         end
