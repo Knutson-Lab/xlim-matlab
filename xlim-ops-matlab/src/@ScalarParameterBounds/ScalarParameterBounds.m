417f0322193bcf84aae9a5ca9485218a5d5dbbf3
shureed <shureed.qazi@nih.gov>
2022-05-17 14:36:32 -0400

    if-else corrections

--- xlim-ops-matlab/src/@ScalarParameterBounds/ScalarParameterBounds.m
+++ xlim-ops-matlab/src/@ScalarParameterBounds/ScalarParameterBounds.m
@@ -23,11 +23,21 @@ classdef ScalarParameterBounds
                 bounds(1:noofBounds) = ScalarParameterBounds;
 
                 if(~isempty(lb) && ~isempty(ub))
+                    assert(all(lb <= ub),"Lower bounds must be less than or equal to corresponding upper bounds")
                     if(nooflb>1 && noofub>1)
                         assert(nooflb==noofub,"No of lower bounds and upper bounds provided not equal")
-                        assert(all(lb <= ub),"Lower bounds must be less than or equal to corresponding upper bounds")
                         for i=1:noofBounds
                             bounds(i).LowerBound = lb(i);
+                            bounds(i).UpperBound = ub(i);
+                        end
+                    elseif(nooflb>1)
+                        for i=1:noofBounds
+                            bounds(i).LowerBound = lb(i);
+                            bounds(i).UpperBound = ub;
+                        end
+                    else
+                        for i=1:noofBounds
+                            bounds(i).LowerBound = lb;
                             bounds(i).UpperBound = ub(i);
                         end
                     end
@@ -42,68 +52,6 @@ classdef ScalarParameterBounds
                 end
             end
         end
-
-%         function bounds = ScalarParameterBounds(args)
-%             % Constructs boundaries for object
-%             arguments
-%                 args.LowerBound (:,1) {mustBeNumeric}
-%                 args.UpperBound (:,1) {mustBeNumeric}
-%             end
-% 
-%             lb = args.LowerBound;
-%             ub = args.UpperBound;
-% 
-%             if(nargin>1)
-%                 noofBounds = numel(lb);
-%                 assert(noofBounds == numel(ub),"No of lower bounds and uppder bounds provided not the same");
-%                 assert(all(lb <= ub),"Lower bounds must be less than or equal to corresponding upper bounds")
-%     
-%                 bounds(1:noofBounds) = ScalarParameterBounds;
-%                 
-%                 for i=1:noofBounds
-%                     bounds(i).LowerBound = lb(i);
-%                     bounds(i).UpperBound = ub(i);
-%                 end
-%             end
-%         end
-
-
-%         function bounds = ScalarParameterBounds(args)
-%             arguments
-%                 args.LowerBound (:,1) {mustBeNumeric} = []
-%                 args.UpperBound (:,1) {mustBeNumeric} = []
-%             end
-%             
-%             noofBounds = max(numel(args.LowerBound),numel(args.UpperBound));
-%             bounds(1:noofBounds) = ScalarParameterBounds;
-% 
-%             if(nargin>1)
-%                 if(~isempty(args.LowerBound) && ~isempty(args.UpperBound))
-%                     assert(noofBounds == numel(args.UpperBound),"No of lower bounds and upper bounds provided not the same");
-%                     assert(all(lb <= ub),"Lower bounds must be less than or equal to corresponding upper bounds")
-%                     
-%                     for i=1:noofBounds
-%                         bounds(i).LowerBound = args.LowerBound(i);
-%                         bounds(i).UpperBound = args.UpperBound(i);
-%                     end
-% 
-%                 elseif(~isempty(args.LowerBound))
-%                     noofBounds = numel(args.LowerBound);
-%                     bounds(1:noofBounds) = ScalarParameterBounds;
-%                     
-%                     for i=1:noofBounds
-%                         bounds(i).LowerBound = args.LowerBound(i);
-%                     end
-%                 else 
-%                     noofBounds = numel(args.UpperBound);
-%                     bounds(1:noofBounds) = ScalarParameterBounds;
-%                     
-%                     for i=1:noofBounds
-%                         bounds(i).UpperBound = args.UpperBound(i);
-%                     end
-%                 end
-%             end           
-%         end
 
         function varargout = multiGet(multibound,prop)
             arguments
