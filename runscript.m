6d52bff81e5f31c058666c5e849fe322a7855811
shureed <shureed.qazi@nih.gov>
2022-06-14 19:40:32 -0400

    code coverage in runscript

--- runscript.m
+++ runscript.m
@@ -13,4 +13,12 @@ xmlFile = "junittestresults.xml";
 p2 = XMLPlugin.producingJUnitFormat(xmlFile);
 runner.addPlugin(p2)
 
+import matlab.unittest.plugins.CodeCoveragePlugin
+import matlab.unittest.plugins.codecoverage.CoberturaFormat
+sourceCodeFile = "xlim-ops-matlab/src/@TcspcData/TcspcData.m";
+reportFile = "cobertura.xml";
+reportFormat = CoberturaFormat(reportFile);
+p3 = CodeCoveragePlugin.forFile(sourceCodeFile,"Producing",reportFormat);
+runner.addPlugin(p3)
+
 results = runner.run(suite);
