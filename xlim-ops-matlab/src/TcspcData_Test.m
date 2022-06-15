ffbd8df3af033c82586459d3f20a3655fa3c1ef3
shureed <shureed.qazi@nih.gov>
2022-06-14 20:57:01 -0400

    verify error test BinWidth

--- xlim-ops-matlab/src/TcspcData_Test.m
+++ xlim-ops-matlab/src/TcspcData_Test.m
@@ -8,5 +8,8 @@ classdef TcspcData_Test < matlab.unittest.TestCase
             testdata = TcspcData("Data",ones(100,1),"BinWidth", 0.5, "PolarizationAngle", 44.7);
             testCase.assertEqual(testdata.Data, ones(100,1));
         end
+        function test_BinWidthError(testCase)
+            testCase.verifyError(@()TcspcData("Data",ones(1,100),"BinWidth","a"),'MATLAB:validators:mustBeNumericOrLogical')
+        end
     end
 end
