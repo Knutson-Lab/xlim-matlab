cb05271736d67320900686c65bb94d204db9e44c
qazisd <shureed.qazi@nih.gov>
2022-06-27 15:49:21 -0400

    assert equal test

--- tests/ops-tests/tcspcinstrument_test.m
+++ tests/ops-tests/tcspcinstrument_test.m
@@ -1,8 +1,13 @@
 classdef tcspcinstrument_test < matlab.unittest.TestCase
     methods(Test)
-        function test_RefLifetime(testCase)
+        function test_RefLifetimeError(testCase)
             testdata = TcspcData("Data",ones(100,1),"BinWidth", 0.5, "PolarizationAngle", 44.7);
             testCase.verifyError(@()TcspcInstrument("Data",testdata,"ReferenceLifetime",-2),'MATLAB:validators:mustBePositive');
+        end
+        function test_IrfData(testCase)
+            testdata = TcspcData("Data",ones(100,1),"BinWidth", 0.5, "PolarizationAngle", 44.7);
+            testirf = TcspcInstrument("Data",testdata);
+            testCase.assertEqual(testirf.Data, testdata);
         end
     end
 end
