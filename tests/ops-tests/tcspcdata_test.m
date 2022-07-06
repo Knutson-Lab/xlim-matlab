c4bf64c2cdf7e4dd79a463fc07c8dc63265dd535
qazisd <shureed.qazi@nih.gov>
2022-07-06 11:22:28 -0400

    proper Error IDs tcspcdata

--- tests/ops-tests/tcspcdata_test.m
+++ tests/ops-tests/tcspcdata_test.m
@@ -8,15 +8,15 @@ classdef tcspcdata_test < matlab.unittest.TestCase
             %data1000(1000) = TcspcData
             %data1000=data1000.multiSet("Data",ones(1000),"BinWidth",0.5*ones(1000,1),"PolarizationAngle",44.7)
             data1000(1000) = TcspcData;
-            testCase.assertError(@()data1000.multiSet("Data",ones(100),"BinWidth",0.5*ones(1000,1),"PolarizationAngle",44.7),'MATLAB:assertion:failed')
+            testCase.assertError(@()data1000.multiSet("Data",ones(100),"BinWidth",0.5*ones(1000,1),"PolarizationAngle",44.7),'TcspcData:multiSet:DataSize')
         end
         function test_multiSetBinWidthError(testCase)
             data1000(1000) = TcspcData;
-            testCase.assertError(@()data1000.multiSet("Data",ones(1000),"BinWidth",0.5*ones(100,1),"PolarizationAngle",44.7*ones(1000,1)),'MATLAB:assertion:failed')
+            testCase.assertError(@()data1000.multiSet("Data",ones(1000),"BinWidth",0.5*ones(100,1),"PolarizationAngle",44.7*ones(1000,1)),'TcspcData:multiSet:BinWidthSize')
         end
         function test_multiSetPolAngError(testCase)
             data1000(1000) = TcspcData;
-            testCase.assertError(@()data1000.multiSet("Data",ones(1000),"BinWidth",0.5*ones(1000,1),"PolarizationAngle",[44.7,44.7]),'MATLAB:assertion:failed')
+            testCase.assertError(@()data1000.multiSet("Data",ones(1000),"BinWidth",0.5*ones(1000,1),"PolarizationAngle",[44.7,44.7]),'TcspcData:multiSet:PolAngSize')
         end
         function test_multiGet_getData(testCase)
             data1000(1000) = TcspcData;
