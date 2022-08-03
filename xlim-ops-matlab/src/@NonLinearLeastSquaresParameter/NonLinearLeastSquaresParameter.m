c02f2f5f268f8a4f4f4a7f2f0baf095843a59b18
qazisd <shureed.qazi@nih.gov>
2022-08-03 13:37:11 -0400

    Error IDs

--- xlim-ops-matlab/src/@NonLinearLeastSquaresParameter/NonLinearLeastSquaresParameter.m
+++ xlim-ops-matlab/src/@NonLinearLeastSquaresParameter/NonLinearLeastSquaresParameter.m
@@ -12,67 +12,6 @@ classdef NonLinearLeastSquaresParameter < OptimizationParameter
 %             param@OptimizationParameter(val,const)
 %         end
 
-%         function param = NonLinearLeastSquaresParameter(val,const)
-%             % NONLINEARLEASTSQUARESPARAMETER
-%             arguments
-%                 val (:,1) FitParameter
-%                 const (:,1) BoundaryConstraints = BoundaryConstraints()
-%             end
-%             if(nargin>1)
-%                 noofval = numel(val);
-%                 noofconst = numel(const);
-%                 assert(noofval == noofconst || noofconst == 1, "Must be equal number of FitParameter objects and BoundaryConstraint objects");
-% 
-%                 values = val.multiGet("Value");
-%                 bounds = const.multiGet("ScalarBounds");
-% 
-%                 [lb,ub] = bounds.multiGet("LowerBound","UpperBound");
-%                 noofub = numel(ub);
-%                 nooflb = numel(lb);
-% 
-%                 if(noofub > 0 && nooflb > 0)
-%                     if(noofub > 1 && nooflb > 1)
-%                         % array of lower bounds and upper bounds
-%                         assert(noofub == nooflb,"Must have same number of lower and upper bounds")
-%                         assert(noofub == noofval,"Each bound must correspond to a value")
-%                     elseif(noofub > 1)
-%                         % 1 lower bound, array of upper bounds
-%                         assert(noofub == noofval,"Each bound must correspond to a value")
-%                     else
-%                         % 1 upper bound, array of lower bounds
-%                         assert(nooflb == noofval,"Each bound must correspond to a value")
-%                     end
-% 
-%                     checklower = all(lb<=values);
-%                     checkupper = all(values<=ub);                
-% 
-%                     assert(checklower && checkupper,"Some Value is not between ScalarBounds")
-%                 elseif (noofub > 0)
-%                     % 1 or array of upper bounds, no lower bound
-%                     assert(noofub == noofval || noofub == 1,"Each bound must correspond to a value")
-%                     checkupper = all(values<=ub);                
-% 
-%                     assert(checkupper,"Some Value is not between ScalarBounds")
-%                 elseif (nooflb > 0)
-%                     % 1 or array of lower bounds, no upper bound
-%                     assert(nooflb == noofval || nooflb == 1,"Each bound must correspond to a value")
-%                     checklower = all(lb<=values);              
-% 
-%                     assert(checklower,"Some Value is not between ScalarBounds")
-%                 end    
-% 
-%                 param(1:noofval) = NonLinearLeastSquaresParameter; 
-%                 for i=1:noofval
-%                     param(i).Value = val(i);
-%                     if(noofconst==1)
-%                         param(i).Constraints = const;
-%                     else 
-%                         param(i).Constraints = const(i);
-%                     end    
-%                 end 
-%             end
-%         end
-
         function multiparam = multiSet(multiparam, args)
 
             arguments
--- xlim-ops-matlab/src/@OptimizationParameter/OptimizationParameter.m
+++ xlim-ops-matlab/src/@OptimizationParameter/OptimizationParameter.m
@@ -18,7 +18,7 @@ classdef OptimizationParameter
             if(nargin>1)
                 noofval = numel(val);
                 noofconst = numel(const);
-                assert(noofval == noofconst || noofconst == 1, "Must be equal number of FitParameter objects and BoundaryConstraint objects");
+                assert(noofval == noofconst || noofconst == 1,'OptimizationParameter:constructor:ValueAndConstraintsNumber', "Must be equal number of FitParameter objects and BoundaryConstraint objects");
 
                 values = val.multiGet("Value");
                 bounds = const.multiGet("ScalarBounds");
@@ -30,32 +30,32 @@ classdef OptimizationParameter
                 if(noofub > 0 && nooflb > 0)
                     if(noofub > 1 && nooflb > 1)
                         % array of lower bounds and upper bounds
-                        assert(noofub == nooflb,"Must have same number of lower and upper bounds")
-                        assert(noofub == noofval,"Each bound must correspond to a value")
+                        assert(noofub == nooflb,'OptimizationParameter:constructor:LowerUpperBoundNumber',"Must have same number of lower and upper bounds")
+                        assert(noofub == noofval,'OptimizationParameter:constructor:ValuesAndBoundsNumber',"Each bound must correspond to a value")
                     elseif(noofub > 1)
                         % 1 lower bound, array of upper bounds
-                        assert(noofub == noofval,"Each bound must correspond to a value")
+                        assert(noofub == noofval,'OptimizationParameter:constructor:ValuesAndUpperBoundsNumber',"Each bound must correspond to a value")
                     else
                         % 1 upper bound, array of lower bounds
-                        assert(nooflb == noofval,"Each bound must correspond to a value")
+                        assert(nooflb == noofval,'OptimizationParameter:constructor:ValuesAndLowerBoundsNumber',"Each bound must correspond to a value")
                     end
 
                     checklower = all(lb<=values);
                     checkupper = all(values<=ub);                
 
-                    assert(checklower && checkupper,"Some Value is not between ScalarBounds")
+                    assert(checklower && checkupper,'OptimizationParameter:constructor:ValueOutsideBounds',"Some Value is not between ScalarBounds")
                 elseif (noofub > 0)
                     % 1 or array of upper bounds, no lower bound
-                    assert(noofub == noofval || noofub == 1,"Each bound must correspond to a value")
+                    assert(noofub == noofval || noofub == 1,'OptimizationParameter:constructor:ValuesAndBoundsNumber',"Each bound must correspond to a value")
                     checkupper = all(values<=ub);                
 
                     assert(checkupper,"Some Value is not between ScalarBounds")
                 elseif (nooflb > 0)
                     % 1 or array of lower bounds, no upper bound
-                    assert(nooflb == noofval || nooflb == 1,"Each bound must correspond to a value")
+                    assert(nooflb == noofval || nooflb == 1,'OptimizationParameter:constructor:ValuesAndBoundsNumber',"Each bound must correspond to a value")
                     checklower = all(lb<=values);              
 
-                    assert(checklower,"Some Value is not between ScalarBounds")
+                    assert(checklower,'OptimizationParameter:constructor:ValueOutsideBounds',"Some Value is not between ScalarBounds")
                 end    
 
                 for i=1:noofval
